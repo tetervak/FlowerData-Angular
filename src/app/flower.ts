4b2a904b00ac555f0c5708ad731e27e67f8236a5
Alex Tetervak <tetervak@sheridancollege.ca>
2024-03-27 10:42:41 -0400

    removed unnecessary "public"

--- src/app/flower.ts
+++ src/app/flower.ts
@@ -1,9 +1,9 @@
 export class Flower {
-  public id: string = '';
-  public name: string = '';
-  public label: string = '';
-  public price: number = 0.0;
-  public description: string = '';
-  public smallImgSrc: string = '';
-  public largeImgSrc: string = '';
+  id: string = '';
+  name: string = '';
+  label: string = '';
+  price: number = 0.0;
+  description: string = '';
+  smallImgSrc: string = '';
+  largeImgSrc: string = '';
 }
