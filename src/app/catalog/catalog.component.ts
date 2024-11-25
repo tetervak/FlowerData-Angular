e61d799bb3f48518925d2ccad56899a50daa555a
Alex Tetervak <tetervak@sheridancollege.ca>
2024-11-25 10:21:21 -0500

    fixed some warnings

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,17 +1,10 @@
 import {Component, EventEmitter, Input, input, Output} from '@angular/core';
-import {Observable, Subscription} from "rxjs";
 import {Flower} from "../flower";
-import {FlowerDataService} from "../flower-data.service";
-import {AsyncPipe, CurrencyPipe} from "@angular/common";
-import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-flower-index',
   standalone: true,
   imports: [
-    AsyncPipe,
-    RouterLink,
-    CurrencyPipe
   ],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
