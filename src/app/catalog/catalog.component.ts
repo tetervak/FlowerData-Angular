12e0eb8caf6ac68b61657bb007b348ffcb1de4e3
Alex Tetervak <tetervak@sheridancollege.ca>
2024-11-25 10:22:28 -0500

    fixed some warnings

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Flower} from "../flower";
 
 @Component({
--- src/app/flower-details/flower-details.component.ts
+++ src/app/flower-details/flower-details.component.ts
@@ -1,15 +1,10 @@
 import {Component, Input} from '@angular/core';
 import {Flower} from "../flower";
-import {Subscription} from "rxjs";
-import {FlowerDataService} from "../flower-data.service";
-import {ActivatedRoute, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-flower-details',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+  imports: [],
   templateUrl: './flower-details.component.html',
   styleUrl: './flower-details.component.css'
 })
